2c339820f026d7fa2cc80451bd5e0823d9b8033c
Jonathan Rein <jrein1296@gmail.com>
2021-03-08 22:33:17 -0500

    Correcting issue with css and js file links.

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,17 +1,14 @@
 // variables
 var startButton = document.querySelector(".start-button");
-var timer = document.querySelector(".timer-");
 // var question = 
 // var answer =
 var userAnswer = document.querySelector("userAnswer");
 var userInitials = document.querySelector("userInitials");
 // var userScore =
+var timer = document.getElementById("timer");
 
 
 
-
-
-// text content
 
 
 
@@ -28,26 +25,42 @@ startButton.addEventListener("click", startGame);
 // functions
 
 
-function startGame() {
-    // isWin = false;
-    // timerCount = 10;
-    // // Prevents start button from being clicked when round is in progress
-    // startButton.disabled = true;
-    // renderBlanks()
-    // startTimer()
-  }
+// function startGame() {
+//     // isWin = false;
+//     // timerCount = 10;
+//     // // Prevents start button from being clicked when round is in progress
+//     // startButton.disabled = true;
+//     // renderBlanks()
+//     // startTimer()
+// }
 
-function take10 (){
-
+function timer(){
+    var timeLeft = 60;
+    var timeInterval = setInterval(function(){
+        if (timeLeft >= 60) {
+            timer.textContent = "Time Left " + timeLeft;
+            timeLeft--;
+        } else if (timeLeft === 0) {
+            timer.textContent = "Time Left " + timeLeft;
+        }
+    }, 1000);
 }
 
-function setScore() {
-    // win.textContent = winCounter;
-    // localStorage.setItem("winCount", winCounter);
-}
+// function askQuestion(){
 
-function resetGame() {
+// }
+
+// function takeTen(){
+
+// }
+
+// function setScore() {
+//     // win.textContent = winCounter;
+//     // localStorage.setItem("winCount", winCounter);
+// }
+
+// function resetGame() {
     
-    // startButton.disabled = false;
+//     // startButton.disabled = false;
 
-  }
+//   }
