6d8ef25858d3eea7bf224c807b6e9b8882161aa1
Jonathan Rein <jrein1296@gmail.com>
2021-03-09 00:08:15 -0500

    Adding placeholder question until questions array is constructed.

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,11 +1,12 @@
 // variables
 var startButton = document.querySelector(".startButton");
-// var question = 
+
 // var answer =
 var userAnswer = document.querySelector("userAnswer");
 var userInitials = document.querySelector("userInitials");
 // var userScore =
-var timer = document.getElementById("timer")
+var timer = document.getElementById("timer");
+var question = document.getElementById("questionBox");
 var timeLeft;
 
 
@@ -17,20 +18,29 @@ var timeLeft;
 
 
 // event listeners
-document.getElementById("startButton").addEventListener("click", countdown);
+document.getElementById("startButton").addEventListener("click", startGame);
 
 
+function startGame() {
+countdown();
+showQuestion();
+}
 
 
 function countdown() {
     var timeLeft = 20;
     var timeInterval = setInterval(function () {
       if (timeLeft > 0) {
-        timer.textContent = timeLeft + ' seconds remaining';
+        timer.textContent = timeLeft + " seconds remaining";
         timeLeft--;
       } else {
-        timer.textContent = timeLeft + ' seconds remaining';
+        timer.textContent = timeLeft + " seconds remaining";
         clearInterval(timeInterval);  
       }
     }, 100);
+  }
+
+  function showQuestion()  {
+      
+         question.textContent = "First Question";
   }
