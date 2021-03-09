4ae1b50470db7e27f3c483658add707630d733bb
Jonathan Rein <jrein1296@gmail.com>
2021-03-08 22:09:40 -0500

    First updates after inside-out build started.

---
+++ assets/js/script.js
@@ -0,0 +1,53 @@
+// variables
+var startButton = document.querySelector(".start-button");
+var timer = document.querySelector(".timer-");
+// var question = 
+// var answer =
+var userAnswer = document.querySelector("userAnswer");
+var userInitials = document.querySelector("userInitials");
+// var userScore =
+
+
+
+
+
+// text content
+
+
+
+
+
+
+
+// event listeners
+startButton.addEventListener("click", startGame);
+
+
+
+
+// functions
+
+
+function startGame() {
+    // isWin = false;
+    // timerCount = 10;
+    // // Prevents start button from being clicked when round is in progress
+    // startButton.disabled = true;
+    // renderBlanks()
+    // startTimer()
+  }
+
+function take10 (){
+
+}
+
+function setScore() {
+    // win.textContent = winCounter;
+    // localStorage.setItem("winCount", winCounter);
+}
+
+function resetGame() {
+    
+    // startButton.disabled = false;
+
+  }
