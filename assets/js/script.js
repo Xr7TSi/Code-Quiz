5cb9c9c02f1c8666fcb4eae9e29382dac80e2ed8
Jonathan Rein <jrein1296@gmail.com>
2021-03-09 12:59:08 -0500

    Adding questionSet varable.

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,15 +1,48 @@
 var startButton = document.querySelector(".startButton");
 var userAnswer = document.querySelector("userAnswer");
-var userInitials = document.querySelector("userInitials");
+var userInitial = document.querySelector("userInitials");
 var timer = document.getElementById("timer");
 var question = document.getElementById("questionBox");
+var answerA = document.getElementById("answerBoxA");
+var answerB = document.getElementById("answerBoxB");
+var answerC = document.getElementById("answerBoxC");
+var answerD = document.getElementById("answerBoxD");
 var timeLeft;
+
+
+var questionSet = [
+{
+  "question": "question 1 here",
+  "answerA": "answer option a here",
+  "answerB": "answer option b here",
+  "answerC": "answer option c here",
+  "answerD": "answer option d here",
+  "correctAnswer": "a",
+},
+{
+  "question": "question 2 here",
+  "answerA": "answer option a here",
+  "answerB": "answer option b here",
+  "answerC": "answer option c here",
+  "answerD": "answer option d here",
+  "correctAnswer": "a",
+},
+{
+  "question": "question 3 here",
+  "answerA": "answer option a here",
+  "answerB": "answer option b here",
+  "answerC": "answer option c here",
+  "answerD": "answer option d here",
+  "correctAnswer": "a",
+},
+]
 
 document.getElementById("startButton").addEventListener("click", startGame);
 
 function startGame() {
 countdown();
 showQuestion();
+showAnswerA();
 }
 
 function countdown() {
@@ -26,5 +59,9 @@ function countdown() {
   }
 
   function showQuestion()  {
-       question.textContent = "First Question";
+       questionBox.textContent = questionSet[0].question;
   }
+
+  function showAnswerA()  {
+    questionBox.textContent = questionSet[0].answerA;
+}
