4518027e321a36178f8241919dc97a1b8ecb3f1e
Jonathan Rein <jrein1296@gmail.com>
2021-03-08 23:30:28 -0500

    Adding functioning countdown timer in js.

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,12 +1,12 @@
 // variables
-var startButton = document.querySelector(".start-button");
+var startButton = document.querySelector(".startButton");
 // var question = 
 // var answer =
 var userAnswer = document.querySelector("userAnswer");
 var userInitials = document.querySelector("userInitials");
 // var userScore =
-var timer = document.getElementById("timer");
-
+var timer = document.getElementById("timer")
+var timeLeft;
 
 
 
@@ -17,50 +17,20 @@ var timer = document.getElementById("timer");
 
 
 // event listeners
-startButton.addEventListener("click", startGame);
+document.getElementById("startButton").addEventListener("click", countdown);
 
 
 
 
-// functions
-
-
-// function startGame() {
-//     // isWin = false;
-//     // timerCount = 10;
-//     // // Prevents start button from being clicked when round is in progress
-//     // startButton.disabled = true;
-//     // renderBlanks()
-//     // startTimer()
-// }
-
-function timer(){
-    var timeLeft = 60;
-    var timeInterval = setInterval(function(){
-        if (timeLeft >= 60) {
-            timer.textContent = "Time Left " + timeLeft;
-            timeLeft--;
-        } else if (timeLeft === 0) {
-            timer.textContent = "Time Left " + timeLeft;
-        }
-    }, 1000);
-}
-
-// function askQuestion(){
-
-// }
-
-// function takeTen(){
-
-// }
-
-// function setScore() {
-//     // win.textContent = winCounter;
-//     // localStorage.setItem("winCount", winCounter);
-// }
-
-// function resetGame() {
-    
-//     // startButton.disabled = false;
-
-//   }
+function countdown() {
+    var timeLeft = 20;
+    var timeInterval = setInterval(function () {
+      if (timeLeft > 0) {
+        timer.textContent = timeLeft + ' seconds remaining';
+        timeLeft--;
+      } else {
+        timer.textContent = timeLeft + ' seconds remaining';
+        clearInterval(timeInterval);  
+      }
+    }, 100);
+  }
